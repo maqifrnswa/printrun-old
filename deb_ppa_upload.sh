757074731075a7fef15604efa0602c88faffbc06
Richard Ulrich <Richard.Ulrich@cubx-software.com>
2011-12-10 23:36:33 +0100

    new version

--- deb_ppa_upload.sh
+++ deb_ppa_upload.sh
@@ -6,7 +6,7 @@ export DEBFULLNAME="Richard Ulrich"
 
 for DISTRIBUTION in precise oneiric natty maverick 
 do
-	PRINTRUNVERSIONSTR=0.0.7~${DISTRIBUTION}
+	PRINTRUNVERSIONSTR=0.0.8~${DISTRIBUTION}
 	sed -i  -e "s/maverick/${DISTRIBUTION}/g" -e "s/natty/${DISTRIBUTION}/g" -e "s/oneiric/${DISTRIBUTION}/g" -e "s/precise/${DISTRIBUTION}/g" debian/changelog
 	dpkg-buildpackage -rfakeroot -S
 	dput ppa:richi-paraeasy/ppa ../printrun_${PRINTRUNVERSIONSTR}_source.changes
